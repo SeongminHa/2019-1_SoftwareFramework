5ff23adea177d22823c485c13d428d855cd3a76f
SeongminHa <49818274+SeongminHa@users.noreply.github.com>
2019-05-17 18:40:09 +0900

    Update JUnit Test class
    give order to test methods & add assertEquals() to check validity

--- MyBatisLab/src/test/java/lab/CustServiceTest.java
+++ MyBatisLab/src/test/java/lab/CustServiceTest.java
@@ -7,9 +7,13 @@ import java.util.Iterator;
 import java.util.List;
 
 import javax.annotation.Resource;
+import javax.sql.DataSource;
 
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 import org.junit.runner.RunWith;
+
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -17,49 +21,99 @@ import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations={"classpath*:META-INF/spring/context-*"})
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class CustServiceTest {
 		
-	@Resource
-	CustomerService cs;
-	CustomerVO vo = new CustomerVO();
+    @Resource(name = "dataSource")
+    DataSource dataSource;
+    @Resource
+    CustomerService cs;
 
-	//Customer 추가 테스트
+	/* Customer INSERT 테스트 */
 	@Test
-	public void testInsertCustomer() throws Exception{
-		CustomerVO vo = new CustomerVO();
-		vo.setId("5");
-		vo.setName("KANG");
-		vo.setAddress("BEIJING");
-		vo.setTel("010-3987-2490");
-		vo.setGender("FEMALE");
-		cs.insertCustomer(vo);
+	public void test1InsertCustomer() throws Exception{
+				
+		CustomerVO vo1 = new CustomerVO();
+		CustomerVO vo2 = new CustomerVO();
+		CustomerVO vo3 = new CustomerVO();
+		CustomerVO vo4 = new CustomerVO();
+
+		vo1.setId("1");
+		vo1.setName("HA");
+		vo1.setAddress("SAN FRANCISCO");
+		vo1.setTel("010-1111-2222");
+		vo1.setGender("FEMALE");
+		cs.insertCustomer(vo1);
+
+		vo2.setId("2");
+		vo2.setName("WI");
+		vo2.setAddress("BEIJING");
+		vo2.setTel("010-3333-4444");
+		vo2.setGender("MALE");
+		cs.insertCustomer(vo2);
+		
+		vo3.setId("3");
+		vo3.setName("HA");
+		vo3.setAddress("SEOUL");
+		vo3.setTel("010-5555-6666");
+		vo3.setGender("FEMALE");
+		cs.insertCustomer(vo3);
+		
+		vo4.setId("4");
+		vo4.setName("KIM");
+		vo4.setAddress("BALENCIA");
+		vo4.setTel("010-7777-8888");
+		vo4.setGender("FEMALE");
+		cs.insertCustomer(vo4);
 	}
-	//Customer SELECT LIST 테스트
+	
+	/* Customer UPDATE 테스트 */
 	@Test
-	public void testSelectCustomerList() throws Exception{
+	public void test2UpdateCustomer() throws Exception{
+		
 		CustomerVO vo = new CustomerVO();
-		vo.setName("KANG");
-		vo.setAddress("BEIJING");
-		System.out.println(cs.selectCustomerList(vo));
-	}
-	//Customer 업데이트 테스트
-	@Test
-	public void testUpdateCustomer() throws Exception{
-		CustomerVO vo = new CustomerVO();
-		vo.setName("PARK");
+		
+		vo.setId("2");
+		vo.setName("LEE");
 		cs.updateCustomer(vo);
 	}
-	//Customer 단일 SELECT 테스트
+	
+	/* Customer DELETE 테스트 */
 	@Test
-	public void testSelectCustomer() throws Exception{
+	public void test3DeleteCustomer() throws Exception{
+		
 		CustomerVO vo = new CustomerVO();
-		System.out.println(cs.selectCustomer(vo));
-	}
-	//Customer 삭제 테스트
-	@Test
-	public void testDeleteCustomer() throws Exception{
-		CustomerVO vo = new CustomerVO();
-		vo.setId("2");
+		
+		vo.setId("4");
 		cs.deleteCustomer(vo);
+	}
+	
+	/* Customer SELECT 테스트 */
+	@Test
+	public void test4SelectCustomer() throws Exception{
+		
+		CustomerVO vo = new CustomerVO();
+		
+		vo.setId("3");
+		CustomerVO result=cs.selectCustomer(vo);
+		System.out.println(result);
+		
+		assertEquals("HA", result.getName());
+		assertEquals("SEOUL", result.getAddress());
+		assertEquals("010-5555-6666", result.getTel());
+		assertEquals("FEMALE", result.getGender());
+	}
+	
+	/* Customer SELECTLIST 테스트 */
+	@Test
+	public void test5SelectCustomerList() throws Exception{
+		
+		CustomerVO vo = new CustomerVO();
+		
+		vo.setName("HA");
+		List <CustomerVO> resultList=cs.selectCustomerList(vo);
+		System.out.println(resultList.size());
+		
+		assertEquals(2, resultList.size());
 	}
 }
